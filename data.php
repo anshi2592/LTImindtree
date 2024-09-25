968638615880ca4c4202d7fb11d29901f079374c
anshi2592 <81504003+anshi2592@users.noreply.github.com>
2024-09-25 16:56:55 +0530

    Update data.php

--- data.php
+++ data.php
@@ -1,1 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ltimindtree"
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$email= POST[email];
+$password= POST[password];
+$sql="INSERT INTO `dataset`(`email`, `password`) VALUES ('$email','$password')";
+?>
